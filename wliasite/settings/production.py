c32d61eaab73ed68ae8e0da9b66bad530da52eb3
ordovezazek <ezekiel.ordoveza@gmail.com>
2020-11-04 07:17:45 +0000

    tempo update

--- wliasite/settings/production.py
+++ wliasite/settings/production.py
@@ -1,6 +1,33 @@
 from .base import *
+from decouple import config, Csv
 
 DEBUG = False
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default=['*'])
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# import sentry_sdk
+# from sentry_sdk.integrations.django import DjangoIntegration
+
+# sentry_sdk.init(
+#     dsn="https://c38f7adc2c214ee8a484d9eb66c3732a@o443729.ingest.sentry.io/5419091",
+#     integrations=[DjangoIntegration()],
+#     traces_sample_rate=1.0,
+
+#     # If you wish to associate users to errors (assuming you are using
+#     # django.contrib.auth) you may enable sending PII data.
+#     send_default_pii=True
+# )
 
 try:
     from .local import *
